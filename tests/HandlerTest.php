dc180445f7fceabf02c5da63ecd4db4f6e67eebf
=TBoileau <t-boileau@email.com>
2018-05-07 23:05:41 +0200

    test travis-ci

---
+++ tests/HandlerTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tboileau-desktop
+ * Date: 07/05/18
+ * Time: 23:04
+ */
+
+namespace TBoileau\FormHandlerBundle\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class HandlerTest extends TestCase
+{
+    public function testCreated()
+    {
+        $this->assertEquals(1,1);
+    }
+}
